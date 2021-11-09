e34f982299959071bf517513fb9d9b5085039313
moosbeere <moosbeere_o@mail.ru>
2021-11-09 13:24:22 +0300

    Get from DB

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -8,7 +8,8 @@ use App\Models\Articles;
 class ArticleController extends Controller
 {
     public function index(){
-        return view('main');
+        $articles = Articles::latest()->get();
+        return view('main', ['letters' => $articles]);
     }
     
     public function contact(){
