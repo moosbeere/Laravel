567ca4f34db2f746f0551d646bb8d1c4f7a8342b
moosbeere <moosbeere_o@mail.ru>
2021-11-11 09:06:18 +0300

    changed views & routes & controllers (refactor from valentin)

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -8,24 +8,28 @@ use App\Models\Articles;
 class ArticleController extends Controller
 {
     public function index(){
-        $bla=[
-            'adres'=>'Большая семеновская',
-            'tel'=>'8(495)232-2323',
-            'email'=>'@mospolitech.ru'
-        ];
+        $aricles = Articles::all();
 
-
-        return view('contact', ['name'=>$bla]);
+        return view('articles.index',['articles'=> $aricles]);
     }
 
     public function create(){
-        $article = new Articles();
-        $article->setAttribute('name', 'tets');
-        $article->setAttribute('shorttext', 'shortdescription');
-        $article->setAttribute('dataCreate', '20.10.2021');
-        return $article->save();
-
+        return view('articles.create');
     }
 
-}
+    public function store(){
+        $article = new Articles();
+        $article->name = request('name');
+        $article->short_text = request('description');
+        $article->data_create = request('date');
+        $article->save();
 
+        return redirect('articles');
+    }
+
+    public function view($id){
+        $article = Articles::findOrFail($id);
+
+        return view('articles.view',['article'=>$article]);
+    }
+}
