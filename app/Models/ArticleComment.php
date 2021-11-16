85c062e31eae3f4e0a7a95a0ffa8a44c609fbc3b
moosbeere <moosbeere_o@mail.ru>
2021-11-16 11:32:28 +0300

    comment:migrate, relationship

--- app/Models/ArticleComment.php
+++ app/Models/ArticleComment.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class ArticleComment extends Model
 {
     use HasFactory;
+    public function article(){
+        return $this->belongsTo(Articles::class);
+    }
+
 }
