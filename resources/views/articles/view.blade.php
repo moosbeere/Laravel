fc3a48d9b6e0e34aa4c0f108efbc04741d2d90c6
moosbeere <moosbeere_o@mail.ru>
2021-11-16 12:18:59 +0300

    comment: controller

--- resources/views/articles/view.blade.php
+++ resources/views/articles/view.blade.php
@@ -3,4 +3,27 @@
     <h3>{{$article->name}}</h3>
     <i>{{$article->data_create}}</i>
     <p>{{$article->short_text}}</p>
+    <br/>
+    @foreach($article->comments as $comment)
+        <b>{{ $comment->title }}</b>
+        <p>{{ $comment->comment }}</p>
+    @endforeach
+
+
+<form method="POST" action="/articles/{{$article->id}}/comment">
+            @csrf
+            <!-- <input type="hidden" name="article_id" value="{{$article->id}}"/> -->
+
+            <div class="mb-3">
+                <label class="form-label">Заголовок комментария</label>
+                <input type="text" name="comment_title" class="form-control"/>
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Комментарий</label>
+                <textarea class="form-control" name="comment" rows="3"></textarea>
+            </div>
+            <button class="btn btn-primary">Отправить</button>
+        </form>
+    </div>
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\ArticleCommentsController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -22,6 +24,23 @@ Route::get('/articles', [ArticleController::class,'index']);
 Route::get('/articles/create', [ArticleController::class,'create']);
 Route::get('/articles/{id}', [ArticleController::class,'view']);
 Route::post('/articles', [ArticleController::class, 'store']);
+Route::post('articles/{id}/comment', [ArticleCommentsController::class, 'store']);
+
+// Route::post('articles/{id}/comment', function ($id){
+//     $article = App\Models\Articles::find($id);
+//     if ($article){
+//         $comment_title = request('comment_title');
+//         $comment = request('comment');
+//         if ($comment && $comment_title){
+//             $new_comment = new App\Models\ArticleComment();
+//             $new_comment->title = $comment_title;
+//             $new_comment->comment = $comment;
+//             $new_comment->article()->associate($article);
+//             $new_comment->save();
+//             return redirect('articles/'.$id);
+//         }
+//     }
+// });
 
 Route::get('/about', function () {
     $contact=[
