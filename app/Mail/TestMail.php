0ffdcdead859008cb4fb8c21f9dc458d6970bc61
Valentin <sosnov@slava.co.uk>
2021-11-30 13:02:31 +0300

    sending email

---
+++ app/Mail/TestMail.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class TestMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    protected $textString;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($textString)
+    {
+        $this->textString = $textString;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('test@polytech.ru')
+            ->to('frozone92wild@gmail.com')
+            ->with([
+                'textString' => $this->textString,
+            ])
+            ->view('mails.test-mail');
+    }
+}
