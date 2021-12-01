22d6e2089f0bf144673c74c60e7df25ca7471209
moosbeere <moosbeere_o@mail.ru>
2021-12-01 20:05:23 +0300

    change route

--- routes/web.php
+++ routes/web.php
@@ -26,22 +26,6 @@ Route::get('/articles/{id}', [ArticleController::class,'view']);
 Route::post('/articles', [ArticleController::class, 'store']);
 Route::post('articles/{id}/comment', [ArticleCommentsController::class, 'store']);
 
-// Route::post('articles/{id}/comment', function ($id){
-//     $article = App\Models\Articles::find($id);
-//     if ($article){
-//         $comment_title = request('comment_title');
-//         $comment = request('comment');
-//         if ($comment && $comment_title){
-//             $new_comment = new App\Models\ArticleComment();
-//             $new_comment->title = $comment_title;
-//             $new_comment->comment = $comment;
-//             $new_comment->article()->associate($article);
-//             $new_comment->save();
-//             return redirect('articles/'.$id);
-//         }
-//     }
-// });
-
 Route::get('/about', function () {
     $contact=[
         'adres'=>'Большая семеновская',
