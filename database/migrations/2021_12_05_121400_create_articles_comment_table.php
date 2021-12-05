4cddb3948580d721ea2b7a2c6b309cd33fa20314
moosbeere <moosbeere_o@mail.ru>
2021-12-05 15:15:50 +0300

    bug with article_comments in db

--- database/migrations/2021_12_05_121400_create_articles_comment_table.php
+++ database/migrations/2021_12_05_121400_create_articles_comment_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateArticleCommentsTable extends Migration
+class CreateArticlesCommentTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateArticleCommentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('article_comments', function (Blueprint $table) {
+        Schema::create('articles_comment', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->text('title');
@@ -29,6 +29,6 @@ class CreateArticleCommentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('article_comments');
+        Schema::dropIfExists('articles_comment');
     }
 }
